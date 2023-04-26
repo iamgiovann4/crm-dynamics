3482fc4afc5a0a54c5f40a3d7cc3ec334949125d
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-04-26 17:54:45 -0300

    modal funcionando  dentro da página produtos

--- src/pages/Produtos.js
+++ src/pages/Produtos.js
@@ -1,17 +1,31 @@
-import AbrirModal from "../components/Model";
+import Modal from 'react-modal';
 import React, { useState } from 'react';
 
+Modal.setAppElement('#root')
+
 function Produtos() {
-    const [exibirComponente, setExibirComponente] = useState(false);
-  
-    const handleClick = () => {
-      setExibirComponente(true);
+    const [abrirModal, setAbrirModal] = useState(false);
+
+    const AbrirModal = () => {
+      setAbrirModal(true)
     }
-  
+
+    const FecharModal = () => {
+      setAbrirModal(false)
+    }
+
     return (
       <div>
-        <button onClick={handleClick}>Exibir componente</button>
-        {exibirComponente && <AbrirModal />}
+        <button onClick={AbrirModal}>Abrir Modal</button>
+
+        <Modal 
+          isOpen={abrirModal}
+          onRequestClose={FecharModal}
+        >
+          <h1>Hello Modal</h1>
+          <p>Testando o modal para o projeto</p>
+          <button onClick={FecharModal}>Fechar</button>
+        </Modal>
       </div>
     );
   }
