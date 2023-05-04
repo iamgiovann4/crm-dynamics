897d3ff535968552af14b26af6a224f0816d63eb
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-04 17:23:55 -0300

    layout cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Logo from "../images/Dynamics.png"
+import Quadrados from "../images/quadrados.png"
+import Form from "../images/form.png"
 import './Cadastro.css'
 
 const Cadastro = () => {
@@ -8,6 +10,7 @@ const Cadastro = () => {
         <>
             <div className='wrapper'>
                 <div className='caixa1'>
+                 <img src={Quadrados} alt="Quadrados" height={'530px'} />
                 </div>
                 <div className="caixa2">
                     <img src={Logo} alt="Mountain" />
@@ -15,6 +18,15 @@ const Cadastro = () => {
                     <div className='welcome'>
                         <h2>Bem Vindo</h2>
                         <h3>Administre sua empresa em uma plataforma <span style={{color:'#0F9AFE'}}>Confiavel</span> </h3>
+
+                        <div>
+                            <img src={Form} alt="Quadrados" height={'350px'} />
+                        </div>
+
+                        <div>
+                            <h2>Já tem uma conta? <a href="#">Entrar</a> </h2>
+                        </div>
+
                     </div>
 
                 </div>
