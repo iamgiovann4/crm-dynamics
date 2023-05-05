535622217e9c3c12301f2547413bd3b3731581fa
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-05-05 16:27:10 -0300

    form da pág cadastro

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -4,17 +4,19 @@ const FormCadastro = () => {
     return(
         <>
             <form className="formCadastro">
-                <div>
+                <div className='nomesCadastro'>
                     <input type="text" name="nome"  placeholder="Nome"/><br/>
                     <input type="text" name="sobrenome"  placeholder="Sobrenome"/><br/>
                 </div>
-               
-                <input type="email" name="email"  placeholder="E-mail"/><br/>
-                <input type="text" name="cpf"  placeholder="CPF"/><br/>
-                <input type="password" name="senha"  placeholder="Senha"/><br/>
-                <input type="password" name="confirmacaoSenha"  placeholder="Confirmar senha"/><br/>
-                <br/>
-                <button className='enviar'>Cadastrar</button><br/>
+
+                <div className='inputsCadastro'>
+                    <input type="email" name="email"  placeholder="E-mail"/><br/>
+                    <input type="text" name="cpf"  placeholder="CPF"/><br/>
+                    <input type="password" name="senha"  placeholder="Senha"/><br/>
+                    <input type="password" name="confirmacaoSenha"  placeholder="Confirmar senha"/><br/>
+                    <br/>
+                    <button className='enviar'>Cadastrar</button><br/>
+                </div>
             </form>
         </>
     )
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Logo from "../images/Dynamics.png"
 import Quadrados from "../images/quadrados.png"
-// import Form from "../images/form.png"
 import './Cadastro.css'
 import FormCadastro from '../components/FormCadastro';
 
@@ -12,24 +11,21 @@ const Cadastro = () => {
             <div className='wrapper'>
                 <div className='caixa1'>
                  <img src={Quadrados} alt="Quadrados" height={'530px'} />
-                </div>
+                </div>{/*  .caixa1 */}
                 <div className="caixa2">
                     <img src={Logo} alt="Logo" />
-                    
                     <div className='welcome'>
                         <h2>Bem Vindo</h2>
                         <h3>Administre sua empresa em uma plataforma <span style={{color:'#0F9AFE'}}>Confiavel</span> </h3>
                         
                         <FormCadastro/>
                         
-                        <div>
+                        <div className='linkLogin'>
                             <h2>Já tem uma conta? <a href="/produtos">Entrar</a></h2>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div>
+                        </div>{/*  .linkLogin */}
+                    </div>{/*  .welcome */}
+                </div>{/*  .caixa2 */}
+            </div>{/*  .wrapper */}
         </>
     );
 };
