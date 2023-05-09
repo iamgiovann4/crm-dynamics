a49336a06486c45360bf3371435a7e6819476eff
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-08 22:18:55 -0300

    criando rota 404

---
+++ src/pages/404.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const pag404 = () => {
+    return(
+        <>
+        <div style={{display:"flex", flexDirection:"column", textAlign:"center"}}>
+            <h1>só temos 2 rotas</h1>
+            <h3>/cadastro</h3>
+            <h3>/produtos</h3>
+        </div>
+            
+        </>
+    );
+};
+
+export default pag404;
