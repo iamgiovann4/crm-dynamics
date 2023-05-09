0b3ff89266e1fa53f1532fb149b90cd89a6fd61a
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-05-09 18:42:13 -0300

    erro na rota encontrado e finalizado

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Cadastro from "./pages/Cadastro";
-import Produtos from "./pages/Produtos";
-import Home from "./pages/home";
+import Products from "./pages/Produtos";
+import Home from "./pages/Home";
 import Pag404 from "./pages/404";
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/produtos" element={<Produtos/>} />
+            <Route path="/produtos" element={<Products/>} />
             <Route path="/cadastro" element={<Cadastro/>} />
             <Route path="/home" element={<Home/>} />
             <Route path="*" element={<Pag404/>} />
