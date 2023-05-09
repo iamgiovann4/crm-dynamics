454f90061b868293a8bfeac45d71bc1095f83bbd
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-08 22:37:42 -0300

    Sidebar na HOME

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './home.css'
+import Sidebar from '../components/Sidebar'
 // import Logo from '../images/Dynamics.png'
 
 const Home = () => {
     return(
         <>
+            <Sidebar/>
         </>
     );
 };
