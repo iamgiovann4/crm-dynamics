f71ef153971f6bc687b782a9947c17c84b4273cd
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-10 17:04:38 -0300

    formulario  usando MUI em andamento

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -5,71 +5,90 @@ import Logo from "../images/Dynamics.png"
 import Quadrados from "../images/quadrados.png"
 import './Cadastro.css'
 import FormCadastro from '../components/FormCadastro';
+import { Grid, Paper, TextField, Box } from '@mui/material';
 
 
 
 //===========================
 export const handleSubmit = async (event) => {
-    event.preventDefault()
-    console.log('Minha funcao de submit')
-    console.log(event.target)
-    const fname = event.target.fname.value 
-    const lname = event.target.lname.value
-    const office = event.target.office.value
-    const cpf = event.target.cpf.value
-    const senha = event.target.password.value
-    const salt = '0912743';
-    const password = sha256(senha + salt);
-    const email = event.target.email.value
-    const user = {fname, lname, office, cpf, password, email}
-    console.log( user)
-    try {
-      const response = await fetch('http://localhost:3100/user',
+  event.preventDefault()
+  console.log('Minha funcao de submit')
+  console.log(event.target)
+  const fname = event.target.fname.value
+  const lname = event.target.lname.value
+  const office = event.target.office.value
+  const cpf = event.target.cpf.value
+  const senha = event.target.password.value
+  const salt = '0912743';
+  const password = sha256(senha + salt);
+  const email = event.target.email.value
+  const user = { fname, lname, office, cpf, password, email }
+  console.log(user)
+  try {
+    const response = await fetch('http://localhost:3100/user',
       {
         method: 'POST',
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(user), 
+        body: JSON.stringify(user),
       })
-      const data = await response.json()
-      console.log(data)
-      if (data.message === "Dados inválidos") {
-        alert('Usuário NÃO cadastrado');
-      } else {
-        alert('Usuário cadastrado');
-      }
-      
-    } catch (error) {
-      console.log("deu merda: " + error)
+    const data = await response.json()
+    console.log(data)
+    if (data.message === "Dados inválidos") {
+      alert('Usuário NÃO cadastrado');
+    } else {
+      alert('Usuário cadastrado');
     }
+
+  } catch (error) {
+    console.log("deu merda: " + error)
   }
+}
 //===========================
 
 const Cadastro = () => {
-    
-    return(
-        <>
-            <div className='wrapper'>
-                <div className='caixa1'>
-                 <img src={Quadrados} alt="Quadrados" height={'530px'} />
-                </div>{/*  .caixa1 */}
-                <div className="caixa2">
-                    <img src={Logo} alt="Logo" />
-                    <div className='welcome'>
-                        <h2>Bem Vindo</h2>
-                        <h3>Administre sua empresa em uma plataforma <span style={{color:'#0F9AFE'}}>Confiavel</span> </h3>
-                        
-                        <FormCadastro/>
-                        
-                        <div className='linkLogin'>
-                            <h2>Já tem uma conta? <a href="/produtos">Entrar</a></h2>
-                        </div>{/*  .linkLogin */}
-                    </div>{/*  .welcome */}
-                </div>{/*  .caixa2 */}
-            </div>{/*  .wrapper */}
-        </>
-    );
-};
+
+  return (
+    <Grid container spacing={2} sx={{ height: '100%' }}>
+      <Grid item xs={4} style={{ backgroundColor: "#252525", display: "flex", alignItems: "center", height: "100%", }}>
+        <img style={styles.img} src={Quadrados} alt={'detalhes'} />
+      </Grid>
+      <Grid item xs={8}>
+        <Box sx={{ paddingTop: '30px', }}>
+          <Box sx={{ display: "flex", justifyContent: "center", }}>
+            <img style={styles} src={Logo} alt={'detalhes'} />
+          </Box>
+          <Box sx={{ height: "100%" }}>
+            <Grid container spacing={2} sx={{ display: "flex", alignItems: "center", height: "100%" }}>
+              <Grid item xs={6}>
+                <TextField label="Nome" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField label="Sobrenome" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="CPF" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="Email" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="Senha" variant="outlined" fullWidth />
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid >
+  );
+}
 
 export default Cadastro;
+
+const styles = {
+  img: {
+    height: "50%",
+    position: "absolute"
+  }
+}
