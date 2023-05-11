3e49df43272460fbcda74c3997690af257f67f83
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-11 17:54:37 -0300

    criei rota de login e tela em andamento

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,14 @@ import Cadastro from "./pages/Cadastro";
 import Products from "./pages/Produtos";
 import Home from "./pages/Home";
 import Pag404 from "./pages/404";
+import Login from "./pages/Login";
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/produtos" element={<Products/>} />
             <Route path="/cadastro" element={<Cadastro/>} />
+            <Route path="/login" element={<Login/>} />
             <Route path="/home" element={<Home/>} />
             <Route path="*" element={<Pag404/>} />
         </Routes>
