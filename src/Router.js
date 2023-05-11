f735e2b4c94966c808043f82bb2a48ae99be4125
vitor490 <104593028+vitor490@users.noreply.github.com>
2023-05-11 17:48:04 -0300

    langpage criada

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import Cadastro from "./pages/Cadastro";
 import Products from "./pages/Produtos";
 import Home from "./pages/Home";
 import Pag404 from "./pages/404";
+import Landpage from "./pages/Landpage";
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
             <Route path="/cadastro" element={<Cadastro/>} />
             <Route path="/home" element={<Home/>} />
             <Route path="*" element={<Pag404/>} />
+            <Route path="/landpage" element={<Landpage/>} />
         </Routes>
     )
 }
---
+++ src/pages/Landpage.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Landpage.css'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Logo from '../images/Dynamics.png'
+import prototipo from '../images/prototipo avançado pag de produtos 7.png'
+import Button from '@mui/material/Button';
+
+
+const Landpage = () => {
+    return(
+        <>
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{boxShadow:'none',}}>
+        <Toolbar sx={{backgroundColor:'#fff',}}>
+          <Typography sx={{ flexGrow: 0.9 }}>
+            <img src={Logo} alt="" />
+          </Typography>
+          <a href="#" style={{textDecoration:'none'}}><strong style={{color:'#000'}}>Entrar</strong></a>
+        </Toolbar>
+      </AppBar>
+    </Box>
+        <Box sx={{
+            width:'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <h2 style={{color:'#000',marginBottom: 0 , fontSize:32}}>Simplifique sua Administração</h2>
+            <h2 style={{color:'#000',marginTop: 0 , fontSize:32}}>com Nossa Plataforma</h2>
+            <h2 style={{color:'#000', marginBottom: 0 , fontSize:20 }}>Com a nossa plataforma de e-commerce, </h2>
+            <h2 style={{color:'#000', marginBottom: 0 , marginTop: 0 , fontSize:20 }}>oferecemos-lhe a oportunidade de criar e gerenciar a</h2>
+            <h2 style={{color:'#000', marginTop: 0 , fontSize:20 }}>sua loja online.</h2>
+            <Button  sx={{width:400, height:60,borderRadius: 10, marginBottom: 5 }}><h2 style={{color:"#fff"}}>Cadastre-se</h2></Button>
+            <img src={prototipo} alt="prototipo" />
+        </Box>
+        </>
+    );
+};
+export default Landpage;
+
