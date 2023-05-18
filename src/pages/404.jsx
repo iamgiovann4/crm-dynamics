726d22305d149b0ede2d737056a1d183e91875d5
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-18 17:02:07 -0300

    Update 404.jsx
    adicionei os link para rotas para ficar mais facil

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const pag404 = () => {
     return(
         <>
         <div style={{display:"flex", flexDirection:"column", textAlign:"center"}}>
-            <h1>só temos 2 rotas</h1>
-            <h3>/cadastro</h3>
-            <h3>/produtos</h3>
-            <h3>/login</h3>
-            <h3>/landpage</h3>
+             <h1>só temos 2 rotas</h1>
+            <Link to={`/cadastro`} ><h3>/cadastro</h3></Link> 
+            <Link to={`/produtos`} ><h3>/produtos</h3></Link> 
+            <Link to={`/login`} ><h3>/cadastro</h3></Link> 
+            <Link to={`/landpage`} ><h3>/landpage</h3></Link> 
         </div>
             
         </>
