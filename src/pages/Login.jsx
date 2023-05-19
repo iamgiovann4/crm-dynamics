5858cd42ff51eb5237718a6d5f0cf61e61646cb3
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-19 14:57:52 -0300

    update Login.jsx
    Adicionei a siljueta em cima do form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, TextField, Box } from '@mui/material';
 
 import Logo from "../images/Dynamics.png"
+import Silhueta from "../images/silhuetaLogin.png"
 import Button from "../components/Button"
 
 
@@ -10,18 +11,23 @@ const Login = () => {
         <>
             <Grid container spacing={2} sx={{ height: '100%', }}>
                 <Grid item xs={12}>
-                    <Box style={{ backgroundColor: "#f5f5f5", display: "flex", alignItems: "center", height: "100%",  flexDirection: "column", justifyContent: "center" }}>
+                    <Box sx={{ backgroundColor: "#f5f5f5", display: "flex", alignItems: "center", flexDirection: "column", justifyContent: "center", paddingTop: "50px" }}>
                         <img style={styles.img} src={Logo} alt={'Logo'} />
-                        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "#fff", padding: "40px", borderRadius: "20px",}}>
-                            <Box> <h1>Silhueta</h1> </Box>
-                            <form style={{ padding:"100px" }}>
+                    </Box>
+                    <Box style={{ backgroundColor: "#f5f5f5", display: "flex", alignItems: "center", height: "100%", flexDirection: "column", justifyContent: "center" }}>
+
+                        <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "#fff", padding: "40px", borderRadius: "20px", }}>
+                            <Box sx={{ marginTop: "-100px" }}>
+                                <img style={styles.silhueta} src={Silhueta} alt={'silhueta'} />
+                            </Box>
+                            <form style={{ padding: "100px" }}>
                                 <Grid item xs={12}>
                                     <TextField id="standard-basic" label="CPF" variant="standard" />
                                 </Grid>
                                 <Grid item xs={12}>
                                     <TextField id="standard-basic" label="Senha" variant="standard" />
                                 </Grid> <br />
-                                <div style={{width:"100%"}}><br /><Button> Entrar </Button></div>
+                                <div style={{ width: "100%" }}><br /><Button> Entrar </Button></div>
                                 {/* <button style={styles.enviar} className='enviar'>Entrar</button><br /> */}
                             </form>
 
@@ -47,6 +53,12 @@ const styles = {
         width: "100%",
         background: "linear-gradient(90deg, #0070C0 0%, rgba(15, 154, 254, 0.7) 100%)",
         boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        margintop: "20px"
+        margintop: "20px",
+    },
+    silhueta: {
+        maxWidth: "100%",
+        height: "auto",
+        padding: 0,
+        margin: 0
     }
 }
