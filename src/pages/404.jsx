3d6ec1d766d6a0b5f51121cbba8dc1150cec11e1
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-05-21 15:10:20 -0300

    Update 404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,7 +5,6 @@ const pag404 = () => {
     return(
         <>
         <div style={{display:"flex", flexDirection:"column", textAlign:"center"}}>
-             <h1>só temos 2 rotas</h1>
             <Link to={`/cadastro`} ><h3>/cadastro</h3></Link> 
             <Link to={`/produtos`} ><h3>/produtos</h3></Link> 
             <Link to={`/login`} ><h3>/login</h3></Link> 
