e52a3c8c28a0ee8a4ea4c425509c49310f625d4b
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-06-01 16:42:07 -0300

    Add rotas de funcionarios e vendas

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,8 @@ import Home from "./pages/Home";
 import Pag404 from "./pages/404";
 import Login from "./pages/Login";
 import Landpage from "./pages/Landpage";
+import Funcionarios from "./pages/Funcionarios";
+import Vendas from "./pages/Vendas";
 
 const Router = () => {
     return (
@@ -16,6 +18,8 @@ const Router = () => {
             <Route path="/login" element={<Login/>} />
             <Route path="/home" element={<Home/>} />
             <Route path="/landpage" element={<Landpage/>} />
+            <Route path="/funcionarios" element={<Funcionarios/>} />
+            <Route path="/vendas" element={<Vendas/>} />
             <Route path="*" element={<Pag404/>} />
             
         </Routes>
