0fd979e027dc96252e9525d34f371456f94d09f5
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-06-01 17:49:47 -0300

    add chart = form

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import './Home.css'
 import Content from '../components/Content';
 import NavBar from '../components/NavBar'
+import { Chart } from '../components/OnlineStore';
 
     const Home = () => {
         return (
 
             <Content title="Dashboard">
                 <NavBar></NavBar>
+
+                <Chart />
             </Content>
 
         );
