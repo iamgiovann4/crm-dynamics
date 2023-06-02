e7d6423fab61562cba09112d99651f24ebe1f42f
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-06-02 17:18:16 -0300

    bug corrido e fundo cinza no cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -61,8 +61,8 @@ const Cadastro = () => {
 
   return (
     <Content>
-    <Grid container spacing={2} sx={{ height: '100%', }}>
-      <Grid item xs={4} style={{ backgroundColor: "#252525", display: "flex", alignItems: "center" }}>
+    <Grid width={"auto"} container spacing={2} sx={{ height: '100%', margin: '0%' }}>
+      <Grid item xs={4} style={{ backgroundColor: "#313131", display: "flex", alignItems: "center" }}>
         <img style={styles.img} src={Quadrados} alt={'detalhes'} />
       </Grid>
       <Grid item xs={8}>
@@ -70,33 +70,33 @@ const Cadastro = () => {
           <Box sx={{ display: "flex", justifyContent: "center", }}>
             <img style={styles.logo} src={Logo} alt={'logo'} />
           </Box>
-          <h2 style={{ fontSize: "34px" }}>Bem Vindo</h2>
-          <h3 > <span style={{ color: "#000000", opacity: "40%" }}>Administre  sua empresa em uma plataforma </span> <span style={{ color: "#0070C0", }}>Confiável</span></h3>
+          <h2 style={{ fontSize: "34px", color: "#252525" }}>Bem-vindo</h2>
+          <h3 ><span style={{ color: "#000", opacity: "50%" }}>Administre  sua empresa em uma plataforma </span> <span style={{ color: "#0070C0", }}>Confiável</span></h3>
           <form onSubmit={handleSubmit} style={{ display: "flex", justifyContent: "center", }}>
             <Grid container spacing={2} sx={{ height: "100%", width: "70%", }}>
               <Grid item xs={6}>
                 <FormControl sx={{ width: '100%' }} variant='outlined'>
-                  <TextField name='lname' label="Nome" variant="outlined" fullWidth />
+                  <TextField name='fname' label="Nome" variant="outlined" fullWidth />
                 </FormControl>
               </Grid>
               <Grid item xs={6}>
                 <FormControl sx={{ width: '100%' }} variant='outlined'>
-                  <TextField name='fname' label="Sobrenome" variant="outlined" fullWidth />
+                  <TextField name='lname' label="Sobrenome" variant="outlined" fullWidth />
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
                 <FormControl sx={{ width: '100%' }} variant='outlined'>
-                  <TextField name='office' label="office" variant="outlined" fullWidth />
+                  <TextField name='office' label="Profissão" variant="outlined" fullWidth />
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
                 <FormControl sx={{ width: '100%' }} variant='outlined'>
-                  <TextField name='cpf' label="cpf" variant="outlined" fullWidth />
+                  <TextField name='cpf' label="CPF" variant="outlined" fullWidth />
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
                 <FormControl sx={{ width: '100%' }} variant='outlined'>
-                  <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                  <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
                   <OutlinedInput
                     id="outlined-adornment-password"
                     type={showPassword ? 'text' : 'password'}
@@ -118,14 +118,14 @@ const Cadastro = () => {
               </Grid>
               <Grid item xs={12}>
                 <FormControl sx={{ width: '100%' }} variant='outlined'>
-                  <TextField name='email' label="email" variant="outlined" fullWidth />
+                  <TextField name='email' label="E-mail" variant="outlined" fullWidth />
                 </FormControl>
               </Grid>
               <Grid item xs={12}>
                 <div style={styles.centralizar}>
                   <div style={{ width: "30%" }}><Button>Cadastrar</Button><br /></div>
 
-                  <h4>Já tem uma conta? <span style={{ color: "#0070C0" }}> <Link to={`/login`} style={{ color: "#0070C0" }}>Entrar</Link> </span> </h4>
+                  <h2 style={{color: '#252525'}}>Já tem uma conta? <span style={{ color: "#0070C0" }}> <Link to={`/login`} style={{ color: "#0070C0", textDecoration: 'none' }}>Entrar</Link> </span> </h2>
                 </div>
 
               </Grid>
