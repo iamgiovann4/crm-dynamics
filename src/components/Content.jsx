bbddffa9046334e768b75a304f5e37af3ad985c7
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-06-06 19:02:35 -0300

    css para fora do arquivo

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,18 @@
 import Box from '@mui/material/Box'
 
-const Content = ({title, children}) => {
-
+const Content = ({children}) => {
     return (
-        <Box sx={{height:'100%', background: "#f1f1f1"}} >
+        <Box style={styles.conteudo}>
             {children}
         </Box>
     )
+}
+
+const styles = {
+    conteudo: {
+        height:'100%', 
+        background: "#f1f1f1"
+    }
 }
 
 export default Content
