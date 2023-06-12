fbe2f10cd451f17bb131ca7442ed4e52065c854e
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-06-12 18:57:57 -0300

    Update TableClient.jsx

--- src/components/TableClient.jsx
+++ src/components/TableClient.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Box from '@mui/material/Box'
 import { FaTrash as IconTrash, FaEdit as IconEdit } from 'react-icons/fa'
-import '../pages/Produto.css'
+// import '../pages/Produto.css'
 import Table from '@mui/material/Table'
 import TableBody from '@mui/material/TableBody'
 import TableCell from '@mui/material/TableCell'
@@ -14,17 +14,33 @@ import Paper from '@mui/material/Paper'
 const TableClient = ({product, setProducts, products}) => {
 
   const [modalOpen, setOpenModal] = useState(false)
-  const [name, setName] = useState(product.name)
-  const [price, setPrice] = useState(product.price)
-  const [stock, setStock] = useState(product.stock)
+  const [fname, setName] = useState(product.fname)
+  const [lname, setPrice] = useState(product.lname)
+  const [cpf, setStock] = useState(product.cpf)
+  const [dateOfBirth, setStock] = useState(product.dateOfBirth)
+  const [phone, setStock] = useState(product.phone)
+  const [email, setStock] = useState(product.email)
+  const [address, setStock] = useState(product.address)
+  const [street, setStock] = useState(product.street)
+  const [cep, setStock] = useState(product.cep)
+  const [houseNumber, setStock] = useState(product.houseNumber)
+  const [referencePoint, setStock] = useState(product.referencePoint)
   
   const handleEdit = async (event) => {
     event.preventDefault()
     const id = parseInt(event.target.id.value)
-    const name = event.target.name.value 
-    const price = event.target.price.value
-    const stock = event.target.stock.value
-    const userEdited = {id, name, price, stock}
+    const fname = event.target.fname.value 
+    const lname = event.target.lprice.value
+    const cpf = event.target.cpf.value
+    const dateOfBirth = event.target.dateOfBirth.value
+    const phone = event.target.phone.value
+    const email = event.target.email.value
+    const address = event.target.address.value
+    const street = event.target.street.value
+    const cep = event.target.cep.value
+    const houseNumber = event.target.houseNumber.value
+    const referencePoint = event.target.referencePoint.value
+    const userEdited = {id, fname, lname, cpf, dateOfBirth, phone, email, address, street, cep, houseNumber, referencePoint}
     try {
       const response = await fetch('http://localhost:3100/product',
       {
@@ -75,10 +91,17 @@ const TableClient = ({product, setProducts, products}) => {
   return (
     <>
       <tr>
-          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.name}</td>
-          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.price}</td>
-          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.stock}</td>
-
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.fname}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.lname}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.cpf}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.dateOfBirth}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.phone}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.email}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.address}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.street}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.cep}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.houseNumber}</td>
+          <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>{product.referencePoint}</td>
           <td style={{borderBottom: '1px solid #ddd',  padding: '15px 23px' }}>
             <IconEdit style={{width: '20px', cursor: 'pointer' }}
             onClick={() => setOpenModal(true)}/>
@@ -99,9 +122,17 @@ const TableClient = ({product, setProducts, products}) => {
             <h2>Editar Produto</h2>
             <form onSubmit={handleEdit} className='formModal'>
               <input type="hidden" name="id" value={product.id} />
-              <input type="text" name="name"  placeholder="Nome" value={name} onChange={e => setName(e.target.value)} /><br/>
-              <input type="text" name="price"  placeholder="Preço" value={price} onChange={e => setPrice(e.target.value)} /><br/>
-              <input type="int" name="stock"  placeholder="Quantidade" value={stock} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="fname"  placeholder="Nome" value={fname} onChange={e => setName(e.target.value)} /><br/>
+              <input type="text" name="lname"  placeholder="Sobrenome" value={lname} onChange={e => setPrice(e.target.value)} /><br/>
+              <input type="text" name="cpf"  placeholder="CPF" value={cpf} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="dateOfBirth"  placeholder="Nascimento" value={dateOfBirth} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="phone"  placeholder="Telefone" value={phone} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="email"  placeholder="E-mail" value={email} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="address"  placeholder="Endereço" value={address} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="street"  placeholder="Bairro" value={street} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="cep"  placeholder="CEP" value={cep} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="houseNumber"  placeholder="N° casa" value={houseNumber} onChange={e => setStock(e.target.value)} /><br/><br/>
+              <input type="text" name="referencePoint"  placeholder="Complemento" value={referencePoint} onChange={e => setStock(e.target.value)} /><br/><br/>
               <button className='enviar' type='submit'>Editar</button><br/>
               <button className='fechar' onClick={() => setOpenModal(false)}>Fechar</button>
             </form>
