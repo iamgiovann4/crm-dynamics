8f17e81d4844b651d44506acec4bd62478fb8c69
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-06-16 14:50:19 -0300

    update graficos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Content from '../components/Content';
-import Header from '../components/Header';
-import Chart from '../components/grafico1';
-import Chart2 from '../components/grafico2';
-import Chart3 from '../components/grafico3';
-import Chart4 from '../components/grafico4';
-import './Home.css'
+import Chart from '../components/Grafico1';
+import Chart2 from '../components/Grafico2';
+import Chart3 from '../components/Grafico3';
+import Chart4 from '../components/Grafico4';
+import './home.css'
 import MiniDrawer from '../components/MiniDrawer'
 
 const Home = () => {
