05c274cfb36579faaa0f92c7ceaebce789da6d5f
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-06-16 17:02:36 -0300

    delete header at Sales.jsx

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,6 +1,5 @@
 //Essa pág listará as vendas feitas. Por isso, precisaremos usar o inner join para obter dados da tabela clientes e produtos, pq nessa lista terá a data da venda, o cliente que comprou e o produto comprado.
 import Content from "../components/Content"
-import Header from "../components/Header"
 import MiniDrawer from '../components/MiniDrawer'
 
 const Sales = () => {
@@ -8,7 +7,7 @@ const Sales = () => {
         <>
             <MiniDrawer>
                 <Content title='Vendas'>
-                    <Header />
+                    
                 </Content>
             </MiniDrawer>
         </>
