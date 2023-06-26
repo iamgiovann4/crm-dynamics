cdf6af9195126f4fc15f7e8d3d20db494b24ea6d
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-06-26 18:20:57 -0300

    listando carrinho de comparas

--- src/Router.js
+++ src/Router.js
@@ -14,6 +14,7 @@ import Searchbar from "./pages/SearchBar";
 import CustomersEdit from "./pages/CustomersEdit";
 import FormEmployees from "./pages/FormEmployees";
 import EmployeesEdit from "./pages/EmployeesEdit";
+import ListSales from "./pages/ListSales";
 
 const Router = () => {
     return (
@@ -30,6 +31,7 @@ const Router = () => {
                 <Route path="/add-funcionarios" element={<FormEmployees />} />
                 <Route path="/cliente-edit" element={<CustomersEdit />} />
                 <Route path="/funcionarios-edit" element={<EmployeesEdit />} />
+                <Route path="/listSales" element={<ListSales />} />
             </Route>
 
             <Route path="/landpage" element={<Landpage />} />
